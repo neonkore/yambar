94dbe3b89261220199cde4abf34429c482aa2e4c
Daniel Eklöf <daniel@ekloef.se>
2019-01-01 20:35:36 +0100

    bar: ignore events we're not interrested in

--- bar.c
+++ bar.c
@@ -622,10 +622,13 @@ run(struct bar_run_context *run_ctx)
             }
 
             case XCB_DESTROY_NOTIFY:
+                LOG_WARN("unimplemented event: XCB_DESTROY_NOTIFY");
+                break;
+
             case XCB_REPARENT_NOTIFY:
             case XCB_CONFIGURE_NOTIFY:
             case XCB_MAP_NOTIFY:
-                LOG_WARN("unimplemented event: %d", XCB_EVENT_RESPONSE_TYPE(e));
+                /* Just ignore */
                 break;
 
             default:
