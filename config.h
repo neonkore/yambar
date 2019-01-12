4dd1a24af692f343a8087f910b3d073c6005d2c0
Daniel Eklöf <daniel@ekloef.se>
2019-01-12 11:47:30 +0100

    config: sort includes, add comment

--- config.h
+++ config.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #include "bar.h"
-#include "yml.h"
 #include "font.h"
 #include "particle.h"
+#include "yml.h"
 
 bool conf_verify_bar(const struct yml_node *bar);
 struct bar *conf_to_bar(const struct yml_node *bar);
+
+/*
+ * Utility functions, for e.g. modules
+ */
 
 struct particle * conf_to_particle(
     const struct yml_node *node, const struct font *parent_font);
